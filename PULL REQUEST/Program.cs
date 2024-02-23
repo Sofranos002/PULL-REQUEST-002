aec229899aa84e7e750876dc2e3f58b875334121
Sohvi Hautajoki <csoha002@edu.xamk.fi>
2024-02-23 13:23:18 +0200

    Add project files.

---
+++ PULL REQUEST/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PULL_REQUEST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Salute warrior");
+        }
+    }
+}
